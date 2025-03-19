a59dee8261ab9b24cc6bde022ba3116190891b1a
prathap <darla.832865@gmail.com>
2025-03-19 20:01:08 +0530

    this is cronjob to get the all info about the AWS

---
+++ mylearning/cronjob.sh
@@ -0,0 +1,84 @@
+#!/bin/bash
+##################################################
+#This is a simple script to start the cronjob
+#
+#Author : PK
+#Date : 18-03-2025
+#Version : V1
+#
+##################################################
+
+#set -x
+
+#Get all the S3 Buckets available
+
+aws s3 ls > Dailytracker.txt
+
+#get all the avaibale IAM Groups
+
+#aws iam list-groups > Dailytracker.txt
+
+#get all the available users 
+#
+
+#aws iam list-users > Dailytracker.txt
+#
+# get all the avaiable EC2 Instances
+#
+#aws ec2 describe-instances > Dailytracker.txt
+
+
+#################################################
+#This is a simple script to start the cronjob
+#
+#Author : PK
+#Date : 18-03-2025
+#Version : V1
+#
+##################################################
+
+#set -x
+
+#Get all the S3 Buckets available
+
+aws s3 ls > Dailytracker.txt
+
+#get all the avaibale IAM Groups
+#aws iam list-groups > Dailytracker.txt
+
+#get all the available users
+#
+
+#aws iam list-users > Dailytracker.txt
+#
+# get all the avaiable EC2 Instances
+#
+#aws ec2 describe-instances > Dailytracker.txt
+##################################################
+#This is a simple script to start the cronjob
+#
+#Author : PK
+#Date : 18-03-2025
+#Version : V1
+#
+##################################################
+
+#set -x
+
+#Get all the S3 Buckets available
+
+aws s3 ls > Dailytracker.txt
+
+#get all the avaibale IAM Groups
+
+#get all the avaibale IAM Groups
+#aws iam list-groups > Dailytracker.txt
+
+#get all the available users
+#
+
+#aws iam list-users > Dailytracker.txt
+#
+# get all the avaiable EC2 Instances
+#
+#aws ec2 describe-instances > Dailytracker.txt
